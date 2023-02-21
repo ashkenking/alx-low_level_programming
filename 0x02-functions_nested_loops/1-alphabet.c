7a1848b21f2ad8d90ecdf058c01db1452717ec6b
ashkenking <ashkenking2000@yahoo.com>
2023-02-21 05:47:14 -0800

    1 aphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*declare the aphabets*/
+/*prints alphabet in lower case*/
+/* declare a new line*/
+
+void print_alphabet(void){
+  char alphabet;
+  for(alphabet = "a"; alphabet <= "z"; alphabet++)
+    
+{
+  _putchar(alphabet);
+}
+
+_putchar('\n');
+    
+}
