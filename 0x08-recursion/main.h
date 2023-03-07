43ca9335b470d3b15160ab33f29905392844345e
ashkenking <ashkenking2000@yahoo.com>
2023-03-07 08:48:58 -0800

    prototype

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,23 +2,20 @@
 #define MAIN_H
 
 /**
- * File: main.h
- * Auth: Adefisoye
+ * File: holberton.h
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-pointers_arrays_strings directory.
+ *       used in the 0x07-recursion directory.
  */
 
-#include <stddef.h>
-
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
